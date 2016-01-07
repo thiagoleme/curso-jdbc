ef6d1e5b9b194bb20ec7d24e1d15b67f0616f765
Thiago Henrique Leme <leme.thiagoh@gmail.com>
2016-01-06 22:44:25 -0200

    Aula 01 - Conexão com Banco de Dados

---
+++ src/test/java/br/com/trilha/java/cursojdbc/TestaListagem.java
@@ -0,0 +1,39 @@
+package br.com.trilha.java.cursojdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class TestaListagem {
+
+	@Test
+	public void RetornaListagem() {
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+
+			Statement statement = connection.createStatement();
+			boolean result = statement.execute("select * from Produto");
+
+			if (result == true) {
+				ResultSet resultSet = statement.getResultSet();
+
+				while (resultSet.next()) {
+					int id = resultSet.getInt("id");
+					String nome = resultSet.getString("nome");
+					String descricao = resultSet.getString("descricao");
+					System.out.println("\n\nID: " + id + "\nNome: " + nome + "\nDescrição: " + descricao);
+				}
+				resultSet.close();
+			}
+			statement.close();
+			connection.close();
+
+		} catch (SQLException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+	}
+}
